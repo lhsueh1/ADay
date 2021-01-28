5077f8ae38e73555dad8cefb3e51954e65a101ea
jerry960331 <jerry960331@gmail.com>
2021-01-28 23:34:51 +0800

    add some switch things

--- app/src/main/java/com/leisure/aday/SwitchActivity.java
+++ app/src/main/java/com/leisure/aday/SwitchActivity.java
@@ -1,17 +1,37 @@
 package com.leisure.aday;
 
 import android.os.Bundle;
-import android.widget.Switch;
+import android.util.Log;
+import android.view.View;
+import android.widget.CompoundButton;
+
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SwitchCompat;
 
 public class SwitchActivity extends AppCompatActivity {
+
+    SwitchCompat switch00;
+    SwitchCompat switch01;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.fragment_home);
 
-        Switch switch00 = findViewById(R.id.switch00);
-        Switch switch01 = findViewById(R.id.switch01);
+
+        switch00 = findViewById(R.id.switch00);
+        switch00.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                switch01.setChecked(switch00.isChecked());
+                System.out.println("test");
+            }
+        });
+
+        switch01 = findViewById(R.id.switch01);
+
+
     }
+
 }
