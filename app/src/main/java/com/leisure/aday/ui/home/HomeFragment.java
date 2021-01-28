852f157f5d8251f68b30cc2be15442eb78cd23e0
jerry960331 <jerry960331@gmail.com>
2021-01-29 00:30:18 +0800

    Make switches unstable.

--- app/src/main/java/com/leisure/aday/ui/home/HomeFragment.java
+++ app/src/main/java/com/leisure/aday/ui/home/HomeFragment.java
@@ -4,7 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Switch;
+import android.widget.CompoundButton;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -22,6 +22,40 @@ public class HomeFragment extends Fragment {
 
     SwitchCompat switch00;
     SwitchCompat switch01;
+    SwitchCompat switch02;
+    SwitchCompat switch03;
+    SwitchCompat switch04;
+    SwitchCompat switch05;
+    SwitchCompat switch06;
+    SwitchCompat switch07;
+    SwitchCompat switch08;
+    SwitchCompat switch09;
+    SwitchCompat switch10;
+    SwitchCompat switch11;
+    SwitchCompat switch12;
+    SwitchCompat switch13;
+    SwitchCompat switch14;
+    SwitchCompat switch15;
+    SwitchCompat switch16;
+    SwitchCompat switch17;
+    SwitchCompat switch18;
+    SwitchCompat switch19;
+    SwitchCompat switch20;
+    SwitchCompat switch21;
+    SwitchCompat switch22;
+    SwitchCompat switch23;
+    SwitchCompat switch24;
+    SwitchCompat switch25;
+    SwitchCompat switch26;
+    SwitchCompat switch27;
+    SwitchCompat switch28;
+    SwitchCompat switch29;
+    SwitchCompat switch30;
+    SwitchCompat switch31;
+    SwitchCompat switch32;
+    SwitchCompat switch33;
+    SwitchCompat switch34;
+    SwitchCompat switch35;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -36,19 +70,207 @@ public class HomeFragment extends Fragment {
             }
         });
 
-        System.out.println("test");
+        findViews(root);
+        setOnCheckedChangeListeners();
 
-        switch00 = root.findViewById(R.id.switch00);
-        switch00.setOnCheckedChangeListener((buttonView, isChecked) -> {
-            if (isChecked) {
-                System.out.println("isCheck = " + isChecked);
-            }
-            switch01.setChecked(switch00.isChecked());
-            System.out.println("test");
-        });
 
-        switch01 = root.findViewById(R.id.switch01);
         return root;
-
     }
+
+    private CompoundButton.OnCheckedChangeListener switchListener = new CompoundButton.OnCheckedChangeListener() {
+        @Override
+        public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+            int randomSwitch = (int) (Math.random() * 36);
+            switch (randomSwitch) {
+                case 0:
+                    switch00.setChecked(isChecked);
+                    break;
+                case 1:
+                    switch01.setChecked(isChecked);
+                    break;
+                case 2:
+                    switch02.setChecked(isChecked);
+                    break;
+                case 3:
+                    switch03.setChecked(isChecked);
+                    break;
+                case 4:
+                    switch04.setChecked(isChecked);
+                    break;
+                case 5:
+                    switch05.setChecked(isChecked);
+                    break;
+                case 6:
+                    switch06.setChecked(isChecked);
+                    break;
+                case 7:
+                    switch07.setChecked(isChecked);
+                    break;
+                case 8:
+                    switch08.setChecked(isChecked);
+                    break;
+                case 9:
+                    switch09.setChecked(isChecked);
+                    break;
+                case 10:
+                    switch10.setChecked(isChecked);
+                    break;
+                case 11:
+                    switch11.setChecked(isChecked);
+                    break;
+                case 12:
+                    switch12.setChecked(isChecked);
+                    break;
+                case 13:
+                    switch13.setChecked(isChecked);
+                    break;
+                case 14:
+                    switch14.setChecked(isChecked);
+                    break;
+                case 15:
+                    switch15.setChecked(isChecked);
+                    break;
+                case 16:
+                    switch16.setChecked(isChecked);
+                    break;
+                case 17:
+                    switch17.setChecked(isChecked);
+                    break;
+                case 18:
+                    switch18.setChecked(isChecked);
+                    break;
+                case 19:
+                    switch19.setChecked(isChecked);
+                    break;
+                case 20:
+                    switch20.setChecked(isChecked);
+                    break;
+                case 21:
+                    switch21.setChecked(isChecked);
+                    break;
+                case 22:
+                    switch22.setChecked(isChecked);
+                    break;
+                case 23:
+                    switch23.setChecked(isChecked);
+                    break;
+                case 24:
+                    switch24.setChecked(isChecked);
+                    break;
+                case 25:
+                    switch25.setChecked(isChecked);
+                    break;
+                case 26:
+                    switch26.setChecked(isChecked);
+                    break;
+                case 27:
+                    switch27.setChecked(isChecked);
+                    break;
+                case 28:
+                    switch28.setChecked(isChecked);
+                    break;
+                case 29:
+                    switch29.setChecked(isChecked);
+                    break;
+                case 30:
+                    switch30.setChecked(isChecked);
+                    break;
+                case 31:
+                    switch31.setChecked(isChecked);
+                    break;
+                case 32:
+                    switch32.setChecked(isChecked);
+                    break;
+                case 33:
+                    switch33.setChecked(isChecked);
+                    break;
+                case 34:
+                    switch34.setChecked(isChecked);
+                    break;
+                case 35:
+                    switch35.setChecked(isChecked);
+            }
+        }
+    };
+
+    private void setOnCheckedChangeListeners() {
+        switch00.setOnCheckedChangeListener(switchListener);
+        switch01.setOnCheckedChangeListener(switchListener);
+        switch02.setOnCheckedChangeListener(switchListener);
+        switch03.setOnCheckedChangeListener(switchListener);
+        switch04.setOnCheckedChangeListener(switchListener);
+        switch05.setOnCheckedChangeListener(switchListener);
+        switch06.setOnCheckedChangeListener(switchListener);
+        switch07.setOnCheckedChangeListener(switchListener);
+        switch08.setOnCheckedChangeListener(switchListener);
+        switch09.setOnCheckedChangeListener(switchListener);
+        switch10.setOnCheckedChangeListener(switchListener);
+        switch11.setOnCheckedChangeListener(switchListener);
+        switch12.setOnCheckedChangeListener(switchListener);
+        switch13.setOnCheckedChangeListener(switchListener);
+        switch14.setOnCheckedChangeListener(switchListener);
+        switch15.setOnCheckedChangeListener(switchListener);
+        switch16.setOnCheckedChangeListener(switchListener);
+        switch17.setOnCheckedChangeListener(switchListener);
+        switch18.setOnCheckedChangeListener(switchListener);
+        switch19.setOnCheckedChangeListener(switchListener);
+        switch20.setOnCheckedChangeListener(switchListener);
+        switch21.setOnCheckedChangeListener(switchListener);
+        switch22.setOnCheckedChangeListener(switchListener);
+        switch23.setOnCheckedChangeListener(switchListener);
+        switch24.setOnCheckedChangeListener(switchListener);
+        switch25.setOnCheckedChangeListener(switchListener);
+        switch26.setOnCheckedChangeListener(switchListener);
+        switch27.setOnCheckedChangeListener(switchListener);
+        switch28.setOnCheckedChangeListener(switchListener);
+        switch29.setOnCheckedChangeListener(switchListener);
+        switch30.setOnCheckedChangeListener(switchListener);
+        switch31.setOnCheckedChangeListener(switchListener);
+        switch32.setOnCheckedChangeListener(switchListener);
+        switch33.setOnCheckedChangeListener(switchListener);
+        switch34.setOnCheckedChangeListener(switchListener);
+        switch35.setOnCheckedChangeListener(switchListener);
+    }
+
+
+    private void findViews(View root) {
+        switch00 = root.findViewById(R.id.switch00);
+        switch01 = root.findViewById(R.id.switch01);
+        switch02 = root.findViewById(R.id.switch02);
+        switch03 = root.findViewById(R.id.switch03);
+        switch04 = root.findViewById(R.id.switch04);
+        switch05 = root.findViewById(R.id.switch05);
+        switch06 = root.findViewById(R.id.switch06);
+        switch07 = root.findViewById(R.id.switch07);
+        switch08 = root.findViewById(R.id.switch08);
+        switch09 = root.findViewById(R.id.switch09);
+        switch10 = root.findViewById(R.id.switch10);
+        switch11 = root.findViewById(R.id.switch11);
+        switch12 = root.findViewById(R.id.switch12);
+        switch13 = root.findViewById(R.id.switch13);
+        switch14 = root.findViewById(R.id.switch14);
+        switch15 = root.findViewById(R.id.switch15);
+        switch16 = root.findViewById(R.id.switch16);
+        switch17 = root.findViewById(R.id.switch17);
+        switch18 = root.findViewById(R.id.switch18);
+        switch19 = root.findViewById(R.id.switch19);
+        switch20 = root.findViewById(R.id.switch20);
+        switch21 = root.findViewById(R.id.switch21);
+        switch22 = root.findViewById(R.id.switch22);
+        switch23 = root.findViewById(R.id.switch23);
+        switch24 = root.findViewById(R.id.switch24);
+        switch25 = root.findViewById(R.id.switch25);
+        switch26 = root.findViewById(R.id.switch26);
+        switch27 = root.findViewById(R.id.switch27);
+        switch28 = root.findViewById(R.id.switch28);
+        switch29 = root.findViewById(R.id.switch29);
+        switch30 = root.findViewById(R.id.switch30);
+        switch31 = root.findViewById(R.id.switch31);
+        switch32 = root.findViewById(R.id.switch32);
+        switch33 = root.findViewById(R.id.switch33);
+        switch34 = root.findViewById(R.id.switch34);
+        switch35 = root.findViewById(R.id.switch35);
+    }
+
+
 }
