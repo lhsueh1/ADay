dcf1eb780e19521639820330778a6f35187a923a
jerry960331 <jerry960331@gmail.com>
2021-02-01 02:48:21 +0800

    測試RecyclerView成功

--- app/src/main/java/com/leisure/aday/ui/slideshow/SlideshowFragment.java
+++ app/src/main/java/com/leisure/aday/ui/slideshow/SlideshowFragment.java
@@ -4,32 +4,70 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.leisure.aday.R;
+import com.leisure.aday.ui.SlideshowRecyclerViewAdapter;
 
-public class SlideshowFragment extends Fragment {
+import java.util.ArrayList;
+
+public class SlideshowFragment extends Fragment implements SlideshowRecyclerViewAdapter.ItemClickListener {
 
     private SlideshowViewModel slideshowViewModel;
+
+    EditText editedText;
+    RecyclerView recyclerView;
+    Button addButton;
+
+    SlideshowRecyclerViewAdapter adapter;
+    ArrayList<String> animalNames = new ArrayList<>();
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         slideshowViewModel =
                 new ViewModelProvider(this).get(SlideshowViewModel.class);
         View root = inflater.inflate(R.layout.fragment_slideshow, container, false);
-        final TextView textView = root.findViewById(R.id.text_slideshow);
-        slideshowViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+        editedText = root.findViewById(R.id.editText);
+        addButton = root.findViewById(R.id.addButton);
+        addButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
+            public void onClick(View v) {
+                animalNames.add(editedText.getText().toString());
+                System.out.println(editedText.getText().toString());
+                adapter.notifyDataSetChanged();
             }
         });
+
+        // data to populate the RecyclerView with
+        animalNames.add("Horse");
+        animalNames.add("Cow");
+        animalNames.add("Camel");
+        animalNames.add("Sheep");
+        animalNames.add("Goat");
+
+        // set up the RecyclerView
+        recyclerView = root.findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        adapter = new SlideshowRecyclerViewAdapter(getActivity(), animalNames);
+        adapter.setClickListener(this);
+        recyclerView.setAdapter(adapter);
+
         return root;
+    }
+
+    @Override
+    public void onItemClick(View view, int position) {
+        Toast.makeText(getActivity(), "You clicked " + adapter.getItem(position) + " on row number " + position, Toast.LENGTH_SHORT).show();
     }
 }
